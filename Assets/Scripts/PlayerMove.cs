4fa15390a82956d4ea795994ecd78e39fdba30f5
OlivierJHebert <jarrod1999@gmail.com>
2020-03-20 18:18:30 -0400

    Refined Player Character Motion
    - increased jump height when holding button
    - increased fall speed
    - replaced character sprite (now has eyes which indicate direction)
    - implemented character "facing" (left & right)
    - smoothed horizontal player acceleration

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    //jumping parameters
+    public float maxJumpTime;//the maximum time for which holding down 'jump' increases jump height
+    private float jumpTimeCounter;
+    private bool isJumping;
+    private bool isFalling;
+    public float fallFactor;//the number by which gravity is multiplied when falling
+    
+    //dash ability parameters
     public float dashStrength = 2f;
     public float dashCooldown = 10f;
     private float timeNextDash = 0.0f;//the timestamp beyond which the player may dash again
 
+    //leap ability parameters
     public float leapStrength = 2f;
     private bool leaping = false;//1 when true, 0 when false
 
     //private Color defaultSpriteColor;
     //public Color leapingSpriteColor;
 
+    //player shapeshift state parameters
+    //TO-DO: move to state mechanism: functions or state design pattern
     private Sprite PlainForm;
     public float p_WalkSpeed, p_JumpSpeed;
     private float p_Grav;
@@ -39,16 +50,23 @@ public class PlayerMove : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //init component references
         m_body = gameObject.GetComponent(typeof(Rigidbody2D)) as Rigidbody2D;
         m_boxCollider2d = gameObject.GetComponent(typeof(BoxCollider2D)) as BoxCollider2D;
         m_spriteRenderer = gameObject.GetComponent(typeof(SpriteRenderer)) as SpriteRenderer;
 
+        //init player shapeshift state
         //defaultSpriteColor = m_spriteRenderer.color;
         PlainForm = m_spriteRenderer.sprite;
         m_WalkSpeed = p_WalkSpeed;
         m_JumpSpeed = p_JumpSpeed;
         p_Material = m_body.sharedMaterial;
         p_Grav = m_body.gravityScale;
+
+        //init jump parameters
+        jumpTimeCounter = 0;
+        isJumping = false;
+        isFalling = false;
     }
 
     // Update is called once per frame
@@ -63,15 +81,55 @@ public class PlayerMove : MonoBehaviour
         //jumping
         if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded())
         {
-            m_body.velocity += Vector2.up * (m_JumpSpeed + (leaping ? leapStrength : 0));
-            leaping = false;
+            m_body.velocity = Vector2.up * (m_JumpSpeed + (leaping ? leapStrength : 0));
+            isJumping = true;
+            jumpTimeCounter = maxJumpTime;
             m_spriteRenderer.color = new Color(255,255,255);//defaultSpriteColor;
         }
+        
+        //if the jump key is held, the player continues to jump for a short time
+        if (Input.GetKey(KeyCode.UpArrow) && isJumping)
+        {
+            if(jumpTimeCounter > 0)
+            {
+                m_body.velocity = Vector2.up * (m_JumpSpeed + (leaping ? leapStrength : 0));
+                jumpTimeCounter -= Time.deltaTime;
+            }
 
+            else
+            {
+                isJumping = false;
+                leaping = false;
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            isJumping = false;
+            leaping = false;
+        }
+        
+        if(m_body.velocity.y < 0)//increase gravity during fall
+        {
+            m_body.velocity += Vector2.up * Physics2D.gravity.y * (fallFactor - 1) * Time.deltaTime;
+        }
+        
         //walking: speed is magnitude (see velocity assignment), axis is direction
-        float h = Input.GetAxisRaw("Horizontal");
+        //Input.GetAxis(...) for smooth movement, Input.GetAxisRaw(...) for snappy movement
+        float walkInput = Input.GetAxis("Horizontal");
 
-        m_body.velocity = new Vector2(h * m_WalkSpeed, m_body.velocity.y);
+        m_body.velocity = new Vector2(walkInput * m_WalkSpeed, m_body.velocity.y);
+
+        //player facing (rotates according to horizontal input)
+        if(walkInput > 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+
+        else if (walkInput < 0)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
 
         // Loop the top and bottom sides of the stage
         if (m_body.position.y > 11) // Top
@@ -126,7 +184,7 @@ public class PlayerMove : MonoBehaviour
         {
             if (Time.time >= timeNextDash)
             {
-                m_body.velocity += new Vector2(h * m_WalkSpeed * dashStrength, 0);
+                m_body.velocity += new Vector2(walkInput * m_WalkSpeed * dashStrength, 0);
                 timeNextDash = Time.time + dashCooldown;
             }
         }

92b34d1dee71b212ba87a2d355f7155507ed964d
OlivierJHebert <jarrod1999@gmail.com>
2020-03-23 16:45:35 -0400

    Player Script Restructure
    - encapsulated shapeshifting into a method and forms into classes
    - mapped player's "jump" to spacebar
    - restuctured "Scripts" folder

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -9,7 +9,7 @@ public class PlayerMove : MonoBehaviour
     private float jumpTimeCounter;
     private bool isJumping;
     public float fallFactor;//the number by which gravity is multiplied when falling
-    
+
     //dash ability parameters
     public float dashStrength = 2f;
     public float dashCooldown = 10f;
@@ -19,32 +19,17 @@ public class PlayerMove : MonoBehaviour
     public float leapStrength = 2f;
     private bool leaping = false;//1 when true, 0 when false
 
-    //private Color defaultSpriteColor;
-    //public Color leapingSpriteColor;
-
     //player shapeshift state parameters
-    //TO-DO: move to state mechanism: functions or state design pattern
-    private Sprite PlainForm;
-    public float p_WalkSpeed, p_JumpSpeed;
-    private float p_Grav;
-    private PhysicsMaterial2D p_Material;
-
-    public Sprite FlatForm;
-    public float f_WalkSpeed, f_JumpSpeed;
-    public float f_Grav;
-    public PhysicsMaterial2D f_Material;
-
-    public Sprite BallForm;
-    public float b_WalkSpeed, b_JumpSpeed;
-    public float b_Grav;
-    public PhysicsMaterial2D b_Material;
+    private Form currentForm;
+    [SerializeField] private Form plainForm;
+    [SerializeField] private Form ballForm;
+    [SerializeField] private Form flatForm;
 
     [SerializeField] private LayerMask platformsLayerMask;
 
     private Rigidbody2D m_body;
     private BoxCollider2D m_boxCollider2d;
     private SpriteRenderer m_spriteRenderer;
-    private float m_WalkSpeed, m_JumpSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -55,12 +40,7 @@ public class PlayerMove : MonoBehaviour
         m_spriteRenderer = gameObject.GetComponent(typeof(SpriteRenderer)) as SpriteRenderer;
 
         //init player shapeshift state
-        //defaultSpriteColor = m_spriteRenderer.color;
-        PlainForm = m_spriteRenderer.sprite;
-        m_WalkSpeed = p_WalkSpeed;
-        m_JumpSpeed = p_JumpSpeed;
-        p_Material = m_body.sharedMaterial;
-        p_Grav = m_body.gravityScale;
+        shapeshift(plainForm);
 
         //init jump parameters
         jumpTimeCounter = 0;
@@ -70,27 +50,27 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Quitting
+        /**********  Quitting  **********/
         if (Input.GetKeyDown(KeyCode.Q))
         {
             Application.Quit();
         }
 
-        //jumping
-        if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded())
+        /**********  Jumping  **********/
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded())
         {
-            m_body.velocity = Vector2.up * (m_JumpSpeed + (leaping ? leapStrength : 0));
+            m_body.velocity = Vector2.up * (currentForm.JumpStrength + (leaping ? leapStrength : 0));
             isJumping = true;
             jumpTimeCounter = maxJumpTime;
             m_spriteRenderer.color = new Color(255,255,255);//defaultSpriteColor;
         }
         
         //if the jump key is held, the player continues to jump for a short time
-        if (Input.GetKey(KeyCode.UpArrow) && isJumping)
+        if (Input.GetKey(KeyCode.Space) && isJumping)
         {
             if(jumpTimeCounter > 0)
             {
-                m_body.velocity = Vector2.up * (m_JumpSpeed + (leaping ? leapStrength : 0));
+                m_body.velocity = Vector2.up * (currentForm.JumpStrength + (leaping ? leapStrength : 0));
                 jumpTimeCounter -= Time.deltaTime;
             }
 
@@ -101,7 +81,7 @@ public class PlayerMove : MonoBehaviour
             }
         }
 
-        if (Input.GetKeyUp(KeyCode.UpArrow))
+        if (Input.GetKeyUp(KeyCode.Space))
         {
             isJumping = false;
             leaping = false;
@@ -111,14 +91,15 @@ public class PlayerMove : MonoBehaviour
         {
             m_body.velocity += Vector2.up * Physics2D.gravity.y * (fallFactor - 1) * Time.deltaTime;
         }
-        
-        //walking: speed is magnitude (see velocity assignment), axis is direction
+
+        /**********  Walking  **********/
+        //speed is magnitude (see velocity assignment), axis is direction
         //Input.GetAxis(...) for smooth movement, Input.GetAxisRaw(...) for snappy movement
         float walkInput = Input.GetAxis("Horizontal");
 
-        m_body.velocity = new Vector2(walkInput * m_WalkSpeed, m_body.velocity.y);
+        m_body.velocity = new Vector2(walkInput * currentForm.WalkSpeed, m_body.velocity.y);
 
-        //player facing (rotates according to horizontal input)
+        //player facing (flips according to horizontal input)
         if(walkInput > 0)
         {
             transform.eulerAngles = new Vector3(0, 0, 0);
@@ -141,38 +122,29 @@ public class PlayerMove : MonoBehaviour
         else if (m_body.position.x > 18) // Right
             m_body.position = new Vector2(-20, m_body.position.y);
 
-        //changing forms
-        if (Input.GetKeyDown(KeyCode.Alpha1) && m_spriteRenderer.sprite != PlainForm)//tap the 1 key to enter PlainForm
+        /**********  Changing Forms  **********/
+        //tap the 1 key to enter PlainForm
+        if (Input.GetKeyDown(KeyCode.Alpha1) && currentForm != plainForm)
         {
-            m_spriteRenderer.sprite = PlainForm;
-            m_WalkSpeed = p_WalkSpeed;
-            m_JumpSpeed = p_JumpSpeed;
-            m_body.gravityScale = p_Grav;
-            m_body.sharedMaterial = p_Material;
+            shapeshift(plainForm);
         }
-
+        
+        //tap the 2 key to enter FlatForm
         //floats, moves slowly, short jump
-        else if(Input.GetKeyDown(KeyCode.Alpha2) && m_spriteRenderer.sprite != FlatForm)//tap the 2 key to enter FlatForm
+        else if(Input.GetKeyDown(KeyCode.Alpha2) && currentForm != flatForm)
         {
-            m_spriteRenderer.sprite = FlatForm;
-            m_WalkSpeed = f_WalkSpeed;
-            m_JumpSpeed = f_JumpSpeed;
-            m_body.gravityScale = f_Grav;
-            m_body.sharedMaterial = f_Material;
+            shapeshift(flatForm);
         }
 
+        //tap the 3 key to enter the BallForm
         //bounces, moves quickly
-        else if (Input.GetKeyDown(KeyCode.Alpha3) && m_spriteRenderer.sprite != BallForm)//tap the 3 key to enter the BallForm
+        else if (Input.GetKeyDown(KeyCode.Alpha3) && currentForm != ballForm)
         {
-            m_spriteRenderer.sprite = BallForm;
-            m_WalkSpeed = b_WalkSpeed;
-            m_JumpSpeed = b_JumpSpeed;
-            m_body.gravityScale = b_Grav;
-            m_body.sharedMaterial = b_Material;
+            shapeshift(ballForm);
         }
 
-        //Abilities
-        else if (Input.GetKeyDown(KeyCode.X) && !leaping && m_spriteRenderer.sprite != FlatForm)//tap S to enter leap state
+        /**********  Abilities  **********/
+        else if (Input.GetKeyDown(KeyCode.X) && !leaping && currentForm == flatForm)//tap S to enter leap state
         {
             leaping = true;
             m_spriteRenderer.color = new Color(0,0,0);//leapingSpriteColor;
@@ -182,7 +154,7 @@ public class PlayerMove : MonoBehaviour
         {
             if (Time.time >= timeNextDash)
             {
-                m_body.velocity += new Vector2(walkInput * m_WalkSpeed * dashStrength, 0);
+                m_body.velocity += new Vector2(walkInput * currentForm.WalkSpeed * dashStrength, 0);
                 timeNextDash = Time.time + dashCooldown;
             }
         }
@@ -194,6 +166,14 @@ public class PlayerMove : MonoBehaviour
         RaycastHit2D raycastHit2d = Physics2D.BoxCast(m_boxCollider2d.bounds.center, m_boxCollider2d.bounds.size, 0f, Vector2.down, .1f, platformsLayerMask);
         Debug.Log(raycastHit2d.collider);
         return raycastHit2d.collider != null;
+    }
+
+    private void shapeshift(Form next)
+    {
+        currentForm = next;
+        m_body.gravityScale = next.GravityScale;
+        m_body.sharedMaterial = next.Material;
+        m_spriteRenderer.sprite = next.Sprite;
     }
 
 }

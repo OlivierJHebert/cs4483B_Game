d09f115f38f5d39064356b01b91f36a283a81cae
Brian <sli867@uwo.ca>
2020-03-27 19:19:22 -0400

    Level Select Stuff

---
+++ Assets/Scripts/LevelSelect/MapManager.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MapManager : MonoBehaviour
+{
+	public Character Character;
+	public Pin StartPin;
+	public Text SelectedLevelText;
+
+	public Pin[] Pins;	
+	
+	/// <summary>
+	/// Use this for initialization
+	/// </summary>
+	private void Start ()
+	{
+		// Pass a ref and default the player Starting Pin
+		int levelComp = PlayerPrefs.GetInt("levelComp", 0);
+		Debug.Log(Pins.Length + "hi");
+
+		for (int i = 0; i < Pins.Length; i++)
+		{
+			if (i < levelComp)
+			{
+				Pins[i].isRed = false;
+				Pins[i].isGreen = true;
+						Debug.Log(i+"owo");
+			}
+			else if (i == levelComp)
+			{	
+				Pins[i].isGreen = false;
+				Pins[i].isRed = true;
+						Debug.Log(i+"douch");
+			}
+			else 
+			{
+				Pins[i].isGreen = false;
+				Pins[i].isRed = false;
+						Debug.Log(i+"dumb");
+			}
+
+		}
+		if(levelComp > 0)
+			Character.Initialise(this, Pins[levelComp-1]);
+		else
+			Character.Initialise(this, StartPin);
+		//DontDestroyOnLoad (transform.gameObject);
+	}
+
+
+	/// <summary>
+	/// This runs once a frame
+	/// </summary>
+	private void Update()
+	{
+		// Only check input when character is stopped
+		if (Character.IsMoving) return;
+		
+		// First thing to do is try get the player input
+		CheckForInput();
+	}
+
+	
+	/// <summary>
+	/// Check if the player has pressed a button
+	/// </summary>
+	private void CheckForInput()
+	{
+		if (Input.GetKeyUp(KeyCode.UpArrow))
+		{
+			Character.TrySetDirection(Direction.Up);
+		}
+		else if(Input.GetKeyUp(KeyCode.DownArrow))
+		{
+			Character.TrySetDirection(Direction.Down);
+		}
+		else if(Input.GetKeyUp(KeyCode.LeftArrow))
+		{
+			Character.TrySetDirection(Direction.Left);
+		}
+		else if(Input.GetKeyUp(KeyCode.RightArrow))
+		{
+			Character.TrySetDirection(Direction.Right);
+		}
+		// Only load level if it is accessible to the player
+		else if(Input.GetKeyUp(KeyCode.Space))
+		{
+			if(Character.CurrentPin.isGreen || Character.CurrentPin.isRed)
+			{
+				SceneManager.LoadScene(Character.CurrentPin.SceneToLoad);
+			}
+			
+		}
+	}
+
+	
+	/// <summary>
+	/// Update the GUI text
+	/// </summary>
+	public void UpdateGui()
+	{
+		SelectedLevelText.text = string.Format("Current Level: {0}", Character.CurrentPin.SceneToLoad);
+	}
+}
---
+++ Assets/Scripts/LevelSelect/Pin.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public enum Direction
+{
+	Up,
+	Down,
+	Left,
+	Right
+}
+
+public class Pin : MonoBehaviour
+{
+	[Header("Options")] //
+	public bool IsAutomatic;
+	public bool HideIcon;
+	public string SceneToLoad;
+	public bool isGreen;
+	public bool isRed;
+	
+	[Header("Pins")] //
+	public Pin UpPin;
+	public Pin DownPin;
+	public Pin LeftPin;
+	public Pin RightPin;
+
+	private Dictionary<Direction, Pin> _pinDirections; 
+	
+	SpriteRenderer renderer;
+
+	/// <summary>
+	/// Use this for initialisation
+	/// </summary>
+	private void Start()
+	{
+		// Load the directions into a dictionary for easy access
+		renderer = GetComponent<SpriteRenderer>();
+
+
+		_pinDirections = new Dictionary<Direction, Pin>
+		{
+			{ Direction.Up, UpPin },
+			{ Direction.Down, DownPin },
+			{ Direction.Left, LeftPin },
+			{ Direction.Right, RightPin }
+		};
+		
+		// Hide the icon if needed
+		if (HideIcon)
+		{
+			GetComponent<SpriteRenderer>().enabled = false;
+		}
+	}
+	
+	public void Update()
+	{
+		if (isGreen) 
+		{
+			renderer.color = new Color(0f, 0.9811321f, 0.05379835f, 1f);
+		}
+		else if (isRed)
+		{
+			renderer.color = new Color(0.9803922f, 0.01919986f, 0f, 1f);
+		}
+	}
+	
+	/// <summary>
+	/// Get the pin in a selected direction
+	/// Using a switch statement rather than linq so this can run in the editor
+	/// </summary>
+	/// <param name="direction"></param>
+	/// <returns></returns>
+	public Pin GetPinInDirection(Direction direction)
+	{
+		switch (direction)
+		{
+			case Direction.Up:
+				return UpPin;
+			case Direction.Down:
+				return DownPin;
+			case Direction.Left:
+				return LeftPin;
+			case Direction.Right:
+				return RightPin;
+			default:
+				throw new ArgumentOutOfRangeException("direction", direction, null);
+		}
+	}
+
+	
+	/// <summary>
+	/// This gets the first pin thats not the one passed 
+	/// </summary>
+	/// <param name="pin"></param>
+	/// <returns></returns>
+	public Pin GetNextPin(Pin pin)
+	{
+		return _pinDirections.FirstOrDefault(x => x.Value != null && x.Value != pin).Value;
+	}
+	
+	
+	/// <summary>
+	/// Draw lines between connected pins
+	/// </summary>
+	private void OnDrawGizmos()
+	{
+		if(UpPin != null) DrawLine(UpPin);
+		if(RightPin != null) DrawLine(RightPin);
+		if(DownPin != null) DrawLine(DownPin);
+		if(LeftPin != null) DrawLine(LeftPin);
+	}
+
+
+	/// <summary>
+	/// Draw one pin line
+	/// </summary>
+	/// <param name="pin"></param>
+	protected void DrawLine(Pin pin)
+	{   
+		Gizmos.color = Color.blue;
+		Gizmos.DrawLine(transform.position, pin.transform.position);
+	}
+}
